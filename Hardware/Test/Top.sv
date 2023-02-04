116ca6f841426a38944037dc023e4dfd07f2ffb6
Ouhznehc <melody@Ouhznehcs-MacBook-Pro.local>
2023-02-04 16:11:45 +0800

    2.4 4:11pm test

--- Hardware/Test/Top.sv
+++ Hardware/Test/Top.sv
@@ -92,6 +92,9 @@ clkgen #(1000)     clkgen_1KHZ(.clkin(CLK100MHZ), .clkout(CLK1KHZ));
 clkgen #(1)        clkgen_1HZ(.clkin(CLK100MHZ), .clkout(CLK1HZ));
 
 //! cpu
+wire clk;
+debounce button(CLK100MHZ, SW[0], clk);
+
 always @(*)
     Hex7Seg = pc;
 initial begin
@@ -106,7 +109,7 @@ always @(posedge CLK50MHZ) begin
 end
 
 cpu my_cpu( 
-    .clock(SW[0]),
+    .clock(clk),
     .reset(reset),
     .instr(instr),
     .data_addr(data_addr),
@@ -166,6 +169,31 @@ keyboard my_keyborad(
 
 endmodule
 
+module debounce(clk, button, out);
+    input clk;
+    input button;
+    output out;
+    reg delay1;
+    reg delay2;
+    reg clk_100ms;
+    reg [31:0] cnt;
+    always@(clk)
+    begin
+        if((cnt + 1) % 5000000 == 0)
+        begin
+            clk_100ms <= ~clk_100ms;
+            cnt <= 0;
+        end
+        else
+            cnt <= cnt + 1;
+    end
+    always@(clk_100ms)
+    begin
+        delay1 <= button;
+        delay2 <= delay1;
+    end
+    assign out = delay1 & delay2;
+endmodule
 
 
 
