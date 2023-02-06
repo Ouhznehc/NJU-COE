0ea9c0a7cc4165354c5a683f4119868fac860ceb
Ouhznehc <melody@Ouhznehcs-MacBook-Pro.local>
2023-02-06 09:40:32 +0800

    2.6 9:41am test

--- Hardware/Test/Top.sv
+++ Hardware/Test/Top.sv
@@ -181,32 +181,6 @@ keyboard my_keyborad(
 
 endmodule
 
-module debounce(clk, button, out);
-    input clk;
-    input button;
-    output out;
-    reg delay1;
-    reg delay2;
-    reg clk_100ms;
-    reg [31:0] cnt;
-    always@(clk)
-    begin
-        if((cnt + 1) % 5000000 == 0)
-        begin
-            clk_100ms <= ~clk_100ms;
-            cnt <= 0;
-        end
-        else
-            cnt <= cnt + 1;
-    end
-    always@(clk_100ms)
-    begin
-        delay1 <= button;
-        delay2 <= delay1;
-    end
-    assign out = delay1 & delay2;
-endmodule
-
 
 
 
