742ad639461649831ad4490a8991e7e6a5fd7695
Ouhznehc <melody@Ouhznehcs-MacBook-Pro.local>
2023-02-07 21:24:53 +0800

    2.7 21:25 software

--- Software/Library/include/klib.h
+++ Software/Library/include/klib.h
@@ -25,6 +25,4 @@ int    atoi      (const char *nptr);
 // stdio.h
 int    printf    (const char *format, ...);
 int    sprintf   (char *str, const char *format, ...);
-int    snprintf  (char *str, size_t size, const char *format, ...);
 int    vsprintf  (char *str, const char *format, va_list ap);
-int    vsnprintf (char *str, size_t size, const char *format, va_list ap);
